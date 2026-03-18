2a040e9bc2041988566d27ae2da0f0c692634cfd
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2026-03-18 17:03:23 +0200

    Extend Prisma Zod schema with custom validation rules

--- src/middleware/userValidator.ts
+++ src/middleware/userValidator.ts
@@ -1,10 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
-import * as z from "zod";
+import { z } from "zod";
 
-import { UserCreateInputObjectSchema } from '../generated/zod/schemas';
+import { UserSchema } from '../generated/zod/schemas';
+
+type CreateUserSchema = z.infer<typeof createUserSchema>;
+
+const createUserSchema = UserSchema.omit({ id: true }).extend({
+  username: z.string().min(3, 'Username must be at least 3 characters long!'),
+  email: z.email('A valid email is required!'),
+  password: z.string().min(6, 'Password must be at least 6 characters long!'),
+  confirmPassword: z.string()
+}).refine(data => data.password === data.confirmPassword, {
+  error: 'The confirmation password must be the same as the password!',
+  path: ['confirmPassword']
+});
 
 const userValidator = (req: Request, res: Response, next: NextFunction) => {
-  const { success, error } = UserCreateInputObjectSchema.safeParse(req.body);
+  const { success, error } = createUserSchema.safeParse(req.body);
   if (!success) {
     return res.status(400).json(z.treeifyError(error));
   }
@@ -12,3 +24,7 @@ const userValidator = (req: Request, res: Response, next: NextFunction) => {
 };
 
 export default userValidator;
+
+export {
+  CreateUserSchema
+}
